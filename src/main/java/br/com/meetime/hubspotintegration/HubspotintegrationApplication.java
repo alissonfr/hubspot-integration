8879047dfc26a0773c86be096ff0455e7ded5283
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2025-03-12 09:20:24 -0300

    initial commit

---
+++ src/main/java/br/com/meetime/hubspotintegration/HubspotintegrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.meetime.hubspotintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubspotintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubspotintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meetime/hubspotintegration/HubspotintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meetime.hubspotintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubspotintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
